220be876a3f754897b130dfff281e3e36d7bf46d
Akhil0202 <66013822+Akhil0202@users.noreply.github.com>
2026-02-20 13:47:02 +0530

    Add Flask app initialization and login route

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import request
+from flask import Flask, request
 import sqlite3
+
+app = Flask(__name__)
+
+conn = sqlite3.connect(":memory:", check_same_thread=False)
+cursor = conn.cursor()
 
 @app.route("/login")
 def login():
     username = request.args.get("u")
     query = "SELECT * FROM users WHERE name='" + username + "'"
     cursor.execute(query)
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(debug=True)
